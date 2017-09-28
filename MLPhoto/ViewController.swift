9efe42a31a6145936467935bac54b359962cf762
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-28 16:54:42 +0900

    Initial Commit

---
+++ MLPhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLPhoto
+//
+//  Created by Stanley Pan on 28/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
